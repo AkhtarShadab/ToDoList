acac5fd84cfe87b407bee4f9c5d8149cdd240c1e
AkhtarShadab <sakn501@gmail.com>
2023-07-05 23:37:53 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,87 @@
-# ToDoList
+# Todo List App
+
+This is a simple web application for managing todo lists. You can use this app to create multiple todo lists, add items to the lists, mark items as completed, and delete items from the lists. The app is built using Node.js, Express.js, and MongoDB.
+
+## Installation
+
+To install and run this app locally, follow these steps:
+
+1. Clone the repository from GitHub:
+
+   ```sh
+   git clone https://github.com/your-username/ToDoList.git
+   ```
+
+2. Change into the project directory:
+
+   ```sh
+   cd ToDoList-app
+   ```
+
+3. Install the dependencies using npm:
+
+   ```sh
+   npm install
+   ```
+
+4. Create a MongoDB Atlas account and set up a new cluster.
+
+5. Replace the MongoDB connection string in the \`app.js\` file with your own connection string. You can find this line in the code:
+
+   ```js
+   mongoose.connect(
+     "mongodb+srv://<USERNAME>:<PASSWORD>@cluster0.83b3qvq.mongodb.net/todoListDB"
+   );
+   ```
+
+6. Start the server:
+
+   ```sh
+   node app.js
+   ```
+
+7. Open your web browser and visit [http://localhost:3000](http://localhost:3000) to access the app.
+
+## Usage
+
+- To create a new todo list, enter the desired list name in the URL (e.g., [http://localhost:3000/work](http://localhost:3000/work)) and press Enter. If the list already exists, it will be loaded; otherwise, a new empty list will be created.
+
+- To add an item to the list, type the item text in the input field and click the "+" button. The item will be added to the bottom of the list.
+
+- To mark an item as completed, click the checkbox next to the item. The item will be removed from the list.
+
+## Contributing
+
+If you'd like to contribute to this project, you can follow these steps:
+
+1. Fork the repository on GitHub.
+
+2. Clone your forked repository:
+
+   ```sh
+   git clone https://github.com/your-username/todo-list-app.git
+   ```
+
+3. Create a new branch for your feature or bug fix:
+
+   ```sh
+   git checkout -b feature/your-feature-name
+   ```
+
+4. Make your changes and commit them with descriptive commit messages:
+
+   ```sh
+   git commit -m "Add your commit message here"
+   ```
+
+5. Push your changes to your forked repository:
+
+   ```sh
+   git push origin feature/your-feature-name
+   ```
+
+6. Open a pull request on the original repository and describe your changes.
+
+## Contact
+
+If you have any questions or suggestions, please feel free to contact me at [sakn501@gmail.com](mailto:sakn501@gmail.com).
---
+++ app.js
@@ -0,0 +1,114 @@
+//Importing modules and creating app
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+const app = express();
+
+//File settings
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+mongoose.connect(
+  "mongodb+srv://<UserName>:<Password>cluster0.83b3qvq.mongodb.net/todoListDB"
+);
+//Schemas
+
+const itemsSchema = {
+  name: String,
+};
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+const Item = mongoose.model("item", itemsSchema);
+const List = mongoose.model("list", listSchema);
+
+async function getItems() {
+  const Items = await Item.find({});
+  return Items;
+}
+
+async function deleteItems(id) {
+  await Item.deleteOne({ _id: id });
+}
+
+async function getList(name) {
+  const Lists = await List.findOne({ name: name });
+  return Lists;
+}
+
+async function findAndupdate(name, id) {
+  const Lists = await List.findOneAndUpdate(
+    { name: name },
+    { $pull: { items: { _id: id } } }
+  );
+  return Lists;
+}
+//URl Direction and Function
+
+app.get("/:customRoute", function (req, res) {
+  const newList = _.capitalize(req.params.customRoute);
+  getList(newList).then(function (FoundLists) {
+    if (!FoundLists) {
+      const list = new List({
+        name: newList,
+        items: [],
+      });
+      list.save();
+      res.redirect("/" + newList);
+    } else {
+      res.render("list", {
+        listTitle: FoundLists.name,
+        newListItem: FoundLists.items,
+      });
+    }
+  });
+});
+
+app.get("/", function (req, res) {
+  //res.sendFile(__dirname + '/index.html');
+  getItems().then(function (FoundItems) {
+    res.render("list", { listTitle: "To-do", newListItem: FoundItems });
+  });
+});
+app.post("/", function (req, res) {
+  const item = req.body.Todo;
+  const ListName = req.body.list;
+
+  if (ListName === "To-do") {
+    if (item !== "") {
+      Item.create({ name: item });
+    }
+    res.redirect("/");
+  } else {
+    const newItem = new Item({
+      name: item,
+    });
+    getList(ListName).then((found) => {
+      found.items.push(newItem);
+      found.save();
+      res.redirect("/" + ListName);
+    });
+  }
+});
+
+//Starting servver at localhost:3000
+app.post("/delete", function (req, res) {
+  const list = req.body.ListType;
+  const id = req.body.checkbox;
+  if (list === "To-do") {
+    deleteItems(id).then(function () {
+      res.redirect("/");
+    });
+  } else {
+    findAndupdate(list, id).then(() => {
+      res.redirect("/" + list);
+    });
+  }
+});
+app.listen("3000", function () {
+  console.log("Server is running");
+});
